363a17507af6c0679d9a4a27ad9952f304904d9f
Asrar Ahammad <asrarahammadshaik@gmail.com>
2026-04-14 23:16:08 +0530

    fix: Change server to bun

---
+++ momnts-api/index.ts
@@ -0,0 +1,10 @@
+import { sql, serve } from "bun";
+
+const server = serve({
+  port: 3000,
+  routes: {
+    "/": new Response("Welcome to Bun"),
+  },
+});
+
+console.log(`Listening on localhost:${server.port}`);
